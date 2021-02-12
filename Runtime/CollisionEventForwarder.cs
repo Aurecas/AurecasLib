b8efbc51f7239445328c8a8ee0ee72c984b393ca
Rotciv Ocnarb <rotciv_ocnarb@hotmail.com>
2021-02-12 15:20:51 -0300

    pasta

--- Runtime/CollisionEventForwarder.cs
+++ Runtime/CollisionEventForwarder.cs
@@ -13,6 +13,15 @@ public class CollisionEventForwarder : MonoBehaviour
     public Action<Collider> TriggerStayEvent;
     public Action<Collider> TriggerExitEvent;
 
+	//2D
+	public Action<Collision2D> CollisionEnterEvent2D;
+    public Action<Collision2D> CollisionStayEvent2D;
+    public Action<Collision2D> CollisionExitEvent2D;
+    
+    public Action<Collider2D> TriggerEnterEvent2D;
+    public Action<Collider2D> TriggerStayEvent2D;
+    public Action<Collider2D> TriggerExitEvent2D;
+
     void OnCollisionEnter(Collision collision) {
         CollisionEnterEvent?.Invoke(collision);
     }
@@ -34,6 +43,31 @@ public class CollisionEventForwarder : MonoBehaviour
     }
     private void OnTriggerExit(Collider other) {
         TriggerExitEvent?.Invoke(other);
+    }
+	
+	//2D
+	
+	void OnCollisionEnter2D(Collision2D collision) {
+        CollisionEnterEvent2D?.Invoke(collision);
+    }
+
+    void OnCollisionStay2D(Collision2D collision) {
+        CollisionStayEvent2D?.Invoke(collision);
+    }
+
+    void OnCollisionExit2D(Collision2D collision) {
+        CollisionExitEvent2D?.Invoke(collision);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        TriggerEnterEvent2D?.Invoke(other);
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        TriggerStayEvent2D?.Invoke(other);
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        TriggerExitEvent2D?.Invoke(other);
     }
 
 }
