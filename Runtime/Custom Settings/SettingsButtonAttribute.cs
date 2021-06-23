d8571f79ae981371f5fa62863ec0401bbeb1c442
Rotciv <rotciv_ocnarb@hotmail.com>
2021-06-23 10:07:12 -0300

    Custom Settings and folder organization

---
+++ Runtime/Custom Settings/SettingsButtonAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public class SettingsButtonAttribute : Attribute
+{
+    public string contextTitle;
+    public string headerContainer;
+
+    public SettingsButtonAttribute(string contextTitle, string headerContainer) {
+        this.contextTitle = contextTitle;
+        this.headerContainer = headerContainer;
+    }
+}
