107aa64a160600ca8f2f97969b62480543adf47d
Rotciv <rotciv_ocnarb@hotmail.com>
2022-03-23 18:41:51 -0300

    return coroutine fix

--- Runtime/Utils/ReturnCoroutine.cs
+++ Runtime/Utils/ReturnCoroutine.cs
@@ -1,29 +1,24 @@
 using System.Collections;
 using UnityEngine;
 
-public class ReturnCoroutine<T> : CustomYieldInstruction {
-    public Coroutine coroutine { get; private set; }
-
-    public override bool keepWaiting {
-        get {
-            bool mn = target.MoveNext();
-            if (target.Current.GetType() == typeof(T)) {
-                result = (T)target.Current;
-            }
-
-            return mn;
-        }
-    }
-
-
-    private T result;
-    private IEnumerator target;
+public class ReturnCoroutine<T> {
+    IEnumerator target;
+    T result;
     public ReturnCoroutine(IEnumerator target) {
         this.target = target;
+    }
+    public IEnumerator Routine() {
+
+        while (target.MoveNext()) {
+            if (target.Current is T) {
+                result = (T)target.Current;
+            }
+            yield return target.Current;
+        }
+
     }
 
     public T GetResult() {
         return result;
     }
-
 }
